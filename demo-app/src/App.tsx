64d076ba5e728c1edbe57f0351f27f4d29f4de28
Michael Coblenz <mcoblenz@ucsd.edu>
2024-10-03 21:35:52 -0700

    Track the number of checked items.

--- demo-app/src/App.tsx
+++ demo-app/src/App.tsx
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, { ChangeEventHandler, MouseEventHandler } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
 
 function App() {
+    const [numRemainingItems, setNumRemainingItems] = useState(0);
+
+
+    function handleCheckboxClick(e: React.ChangeEvent<HTMLInputElement>) {
+        const checkbox: HTMLInputElement = e.target as HTMLInputElement;
+
+        const diff = checkbox.checked ? 1 : -1;
+
+        setNumRemainingItems(numRemainingItems + diff);
+    }
+
     return (
         <div className="App">
             {/* <header className="App-header">
             </header> */}
             <div className="App-body">
+                Items to buy: {numRemainingItems}
                 <form action=".">
                     <ol>
-                        {[ListItem("Apples"),
-                        ListItem("Bananas"),
-                        ListItem("Oranges")]}
+                        {[ListItem("Apples", handleCheckboxClick),
+                        ListItem("Bananas", handleCheckboxClick),
+                        ListItem("Oranges", handleCheckboxClick)]}
                     </ol>
                 </form>
             </div>
@@ -21,9 +35,9 @@ function App() {
 }
 
 
-function ListItem(itemText: string) {
+function ListItem(itemText: string, changeHandler: ChangeEventHandler) {
     return (
-        <li><input type="checkbox" />{itemText}</li>
+        <li key={itemText}><input type="checkbox" onChange={changeHandler} />{itemText}</li>
     )
 }
 
