be8b0fcb4a1f8344a7729219ccc545c54c56805c
Michael Coblenz <mcoblenz@ucsd.edu>
2024-10-03 21:53:01 -0700

    Sort items according to whether they've been purchased.

--- demo-app/src/App.tsx
+++ demo-app/src/App.tsx
@@ -3,13 +3,31 @@ import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 
+type GroceryItem = { name: string, isPurchased: boolean };
 
 function App() {
     const [numRemainingItems, setNumRemainingItems] = useState(0);
 
+    let [items, setItems] = useState([
+        { name: "Apples", isPurchased: false },
+        { name: "Bananas", isPurchased: false }
+    ]);
+
 
     function handleCheckboxClick(e: React.ChangeEvent<HTMLInputElement>) {
         const checkbox: HTMLInputElement = e.target as HTMLInputElement;
+
+        const itemName = checkbox.name;
+
+        const itemIndex = items.findIndex((item) => item.name === itemName);
+        items[itemIndex] = { name: itemName, isPurchased: checkbox.checked };
+
+        const uncheckedItems = items.filter((item) => !item.isPurchased);
+        const checkedItems = items.filter((item) => item.isPurchased);
+
+        const newItems = uncheckedItems.concat(checkedItems);
+
+        setItems(newItems);
 
         const diff = checkbox.checked ? 1 : -1;
 
@@ -24,9 +42,7 @@ function App() {
                 Items to buy: {numRemainingItems}
                 <form action=".">
                     <ol>
-                        {[ListItem("Apples", handleCheckboxClick),
-                        ListItem("Bananas", handleCheckboxClick),
-                        ListItem("Oranges", handleCheckboxClick)]}
+                        {items.map((item) => ListItem(item, handleCheckboxClick))}
                     </ol>
                 </form>
             </div>
@@ -35,9 +51,9 @@ function App() {
 }
 
 
-function ListItem(itemText: string, changeHandler: ChangeEventHandler) {
+function ListItem(item: GroceryItem, changeHandler: ChangeEventHandler) {
     return (
-        <li key={itemText}><input type="checkbox" onChange={changeHandler} />{itemText}</li>
+        <li key={item.name}><input type="checkbox" onChange={changeHandler} checked={item.isPurchased} name={item.name} />{item.name}</li>
     )
 }
 
